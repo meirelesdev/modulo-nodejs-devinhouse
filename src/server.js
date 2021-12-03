bc24a26252d7d2ba7990e4803a59280819dd6536
Daniel Meireles <daniel@inovadora.com.br>
2021-12-03 08:37:08 -0300

    adicionando o multer ao projeto

--- src/server.js
+++ src/server.js
@@ -1,17 +1,30 @@
 const express = require('express')
-const { createFolder } = require('./utils')
+const cors = require('cors')
+const multer = require('multer')
 
+const { createFolder } = require('./utils')
 const app = express()
+
+const upload = multer()
+
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res)=>{
     res.status(200).json({message:'funcioando...'})
 })
-app.post('/', (req, res)=>{
-    const { name } = req.body
-    console.log(name)
-    const msg = createFolder(name)
-    res.status(201).json({message:msg})
+
+app.post('/', upload.single('file'), (req, res)=>{
+    console.log(req.file)
+    res.status(200).json({message:'funcioando...'})
 })
 
-app.listen(5000,()=>console.log('rodando...'))
+app.post('/save', (req, res)=>{
+    const { name } = req.body
+    console.log(req.body)
+    const msg = createFolder(`src/${name}`)
+
+    res.status(201).json({message:msg})
+})
+const port = 5000
+app.listen(port,()=>console.log('Server rodando na porta '+ port))
