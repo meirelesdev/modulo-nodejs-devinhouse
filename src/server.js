8a4a949ca2a8954c9d3fd58a27dcd655ad8145d7
Daniel Meireles <daniel@inovadora.com.br>
2021-12-05 12:49:49 -0300

    Exercicio 02 Finalizado

--- src/server.js
+++ src/server.js
@@ -1,22 +1,66 @@
+const fileSystem = require('fs')
+
 const express = require('express')
 const cors = require('cors')
 const multer = require('multer')
 
-const { createFolder } = require('./utils')
-const app = express()
+const { createFolder, readUsers, orderData, getArrayDaysOfMonth } = require('./utils')
 
+const app = express()
 const upload = multer()
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res)=>{
-    res.status(200).json({message:'funcioando...'})
+// [M2S02] Ex 1 - Ordenando a posição de itens
+app.patch('/users', (req, res)=>{
+    const data = req.body    
+    if(Object.keys(data).length === 0) {
+        res.status(404).json({message:'Sem dados para processar.', })
+        return
+    }
+    console.log(data)
+    const orderedData = orderData(data)
+    res.status(200).json({message:'sucesso', data: orderedData})
+})
+
+// [M2S02] Ex 2 - Gerando datas
+app.get('/daysofmonth/:mes', (req, res)=>{
+    const { mes } = req.params;
+    if(!mes) {
+        res.status(404).json({message:'Informe um mes'})
+        return    
+    }
+    const daysOfMonth = getArrayDaysOfMonth(mes)
+    if(daysOfMonth.length === 0 ) {
+        res.status(404).json({message:'Mês informado invalido.'})
+        return    
+    }
+    res.status(200).json({message:'sucesso', data: daysOfMonth})
+})
+
+app.get('/users', (req, res)=>{
+    const users = readUsers();
+    res.status(200).json({message:'sucesso', data: users})
 })
 
 app.post('/', upload.single('file'), (req, res)=>{
-    console.log(req.file)
-    res.status(200).json({message:'funcioando...'})
+    const { folder, item } = req.body
+    const { job } = req.query
+    const existFolder = createFolder(folder)
+    delete req.body.folder
+
+    //Este if verifica se existe o arquivo user.json
+    if (fileSystem.lstatSync('src/'+'user.json').isFile()) {
+        //Neste result, retorna os dados do JSON de dentro do user
+        const result = JSON.parse(fileSystem.readFileSync('src/'+'user.json', 'utf8'));
+        console.log(result)
+        return res.status(201).json({message: 'Caiu aqui'})
+    }
+    //Nesta Linha de baixo, é criado o user.json e preenche o arquivo JSON de acordo com o que foi enviado no req.body
+    fileSystem.writeFileSync('src/'+'user.json', JSON.stringify(req.body));
+
+    return res.status(201).json({message: 'Hello world'})
 })
 
 app.post('/save', (req, res)=>{
--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,35 @@ const createFolder = (folderName) => {
     }
     return `Pasta ${folderName} ja existe.`
 }
+const orderData = (nameToChange, data ) =>{
+    
+}
+const readUsers = ()=>{
+    return JSON.parse(fileSystem.readFileSync('src/'+'user.json', 'utf8'));
+}
+function getDaysOfMonth(month, year) {
+    month--;
+    var date = new Date(year, month, 1);
+    var days = [];
+    while (date.getMonth() === month) {
+       days.push(date.getDate());
+       date.setDate(date.getDate() + 1);
+    }
+    return days;
+}
 
+const getArrayDaysOfMonth = (month) => {
+    let response = [] 
+    let options = getDaysOfMonth(month, (new Date()).getFullYear())
+    for(let i= 0; i < options.length ; i++){
+        const date = new Date( (new Date()).getFullYear(), (month-1),options[i])
+        response.push(date.toLocaleString('pt-BR', {dateStyle: 'short'}))
+    }
+    return response
+}
 module.exports = {
-    createFolder
+    createFolder,
+    readUsers,
+    orderData,
+    getArrayDaysOfMonth
 }
