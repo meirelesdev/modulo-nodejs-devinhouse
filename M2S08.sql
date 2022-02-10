314ed1006893bfa94eba8a87ca3949354606f238
Daniel Meireles <daniel@inovadora.com.br>
2022-02-10 07:35:17 -0300

    Finalizando exercicio 2

--- M2S08.sql
+++ M2S08.sql
@@ -3,5 +3,13 @@ CREATE SCHEMA food4dev;
 SET SEARCH_PATH TO 'food4dev';
 SELECT * FROM pratos p ;
 SELECT r.categoria_id, r.categoria_old, * FROM restaurantes r;
-ALTER table restaurantes drop column categoria_old ;
+ALTER TABLE restaurantes DROP column categoria_old ;
 
+-- [M2S08] Ex 2 - Continuação do Food4Devs
+CREATE TABLE pedidos (
+    id SERIAL PRIMARY KEY,
+    numero_pedido INT NOT NULL,
+    valor_entrega DECIMAL(5,2) NOT NULL CHECK(valor_entrega >= 0),
+    data_pedido TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    restaurante_id INT NOT NULL REFERENCES restaurantes(id)
+);
