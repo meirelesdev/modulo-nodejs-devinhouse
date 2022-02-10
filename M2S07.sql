f1c1c84ef8dc8d88242b93b6a6e1d0b4c178eef0
Daniel Meireles <daniel@inovadora.com.br>
2022-02-10 07:23:29 -0300

    Ajuste no nome dos arquivos.

--- M2S07.sql
+++ M2S07.sql
@@ -1,7 +1,143 @@
--- [M2S08] Ex 1 - Continuação do Food4Devs
+-- [M2S07] Ex 1 - Introdução e primeira tabela
 CREATE SCHEMA food4dev;
 SET SEARCH_PATH TO 'food4dev';
-SELECT * FROM pratos p ;
-SELECT r.categoria_id, r.categoria_old, * FROM restaurantes r;
-ALTER table restaurantes drop column categoria_old ;
+CREATE TABLE restaurante(
+    nome VARCHAR(80),
+    telefone VARCHAR(15),
+    endereco VARCHAR(150),
+    categoria VARCHAR(80)
+);
+SELECT * from restaurante;
+-- [M2S07] Ex 2 - Definição da segunda tabela
+CREATE TABLE prato(
+    titulo VARCHAR(80),
+    descricao VARCHAR(300),
+    preco NUMERIC(5,2)
+);
+-- [M2S07] Ex 3 - Inserindo dados
+INSERT INTO restaurante (nome, telefone, endereco, categoria) 
+VALUES
+('Comidas Tradicionais', '(21)99012-1212', 'Rua jurandir Nº50, Bairro damaceno, Cidade: Rio de Janeiro, UF: RJ', 'Comida brasileira'),
+('Burger King', '(48)99012-3456', 'Rua presidente kennedy Nº446, Bairro Kobrasol, Cidade: São José, UF: SC', 'Lanchonete'),
+('Pizza Kut', '(48)99012-0000', 'Rua das borboletas Nº43, Bairro Cento, Cidade: Florianópolis, UF: SC', 'Pizzaria'),
+('Curry Pasta', '(41)99000-1111', 'Av Manoel Ribas  Nº750, Bairro Boa vista, Cidade: Curitiba, UF: PR', 'Comida brasileira'),
+('Gugu Lanches', '(51)98765-4321', 'Rua São Luiz Nº50, Bairro damaceno, Cidade: Porto Alegre, UF: RS', 'Lanchonete');
+
+SELECT * FROM restaurante;
+
+ALTER TABLE food4dev.prato ALTER COLUMN descricao TYPE text;
+INSERT INTO prato (titulo, descricao, preco)
+VALUES
+('Marmita da Hora', 'Prato tradicional arroz, feijao, batata, bife', 10.99),
+('Whopper Costela', 'O pão com gergelim do WHOPPER®, nossa maionese de fabricação própria, alface e tomate cortados na hora, aquela nossa cebola crocante como toda Onion Rings deve ser, junto do nosso delicioso molho Furioso, queijo e a grande novidade: um hambúrguer de carne de porco com aquele aroma inconfundível de Costelinha', 29.90),
+('Pizza Combo', 'Pizza três sabores a escolha do cliente, refri e broto doce', 59.90),
+('Macarronada italiana', 'Deliciosa massa italiana acompanha vinho tinto', 35.90),
+('X-Bacon', 'Delicioso lanche com carne salada e bacon', 25.90);
+SELECT * FROM prato;
+-- [M2S07] Ex 4 - Alterando tabelas - Restaurante
+ALTER TABLE food4dev.restaurante ADD COLUMN endereco_rua VARCHAR(80);
+ALTER TABLE food4dev.restaurante ADD COLUMN endereco_numero INT;
+ALTER TABLE food4dev.restaurante ADD COLUMN endereco_comp VARCHAR(80);
+ALTER TABLE food4dev.restaurante ADD COLUMN endereco_bairro VARCHAR(80);
+ALTER TABLE food4dev.restaurante ADD COLUMN endereco_cidade VARCHAR(80);
+ALTER TABLE food4dev.restaurante ADD COLUMN endereco_estado VARCHAR(80);
+
+-- [M2S07] Ex 5 - Alterando tabelas - Pratos
+ALTER TABLE food4dev.prato add COLUMN categorias VARCHAR(80);
+ALTER TABLE food4dev.prato add COLUMN url_foto TEXT;
+SELECT * from food4dev.prato;
+
+-- [M2S07] Ex 6 - Normalizando a tabela
+ALTER TABLE food4dev.restaurante ADD COLUMN id serial PRIMARY KEY;
+ALTER TABLE food4dev.restaurante ADD COLUMN url_foto VARCHAR(255) DEFAULT 'https://via.placeholder.com/150 ';
+
+
+UPDATE food4dev.restaurante SET endereco_rua = 'Rua jurandir' WHERE id = 1;
+UPDATE food4dev.restaurante SET endereco_bairro = 'Damaceno' WHERE id = 1;
+UPDATE food4dev.restaurante SET endereco_numero = 50 WHERE id = 1;
+UPDATE food4dev.restaurante SET endereco_cidade = 'Rio de janeiro' WHERE id = 1;
+UPDATE food4dev.restaurante SET endereco_estado = 'Rio de janeiro' WHERE id = 1;
+
+UPDATE food4dev.restaurante SET endereco_rua = 'Rua presidente kennedy' WHERE id = 2;
+UPDATE food4dev.restaurante SET endereco_numero = 446 WHERE id = 2;
+UPDATE food4dev.restaurante SET endereco_bairro = 'Kobrasol' WHERE id = 2;
+UPDATE food4dev.restaurante SET endereco_cidade = 'São jose' WHERE id = 2;
+UPDATE food4dev.restaurante SET endereco_estado = 'Santa catarina' WHERE id = 2;
+
+UPDATE food4dev.restaurante SET endereco_rua = 'Rua das borboletas'  WHERE id = 3;
+UPDATE food4dev.restaurante SET endereco_numero = 43 WHERE id = 3;
+UPDATE food4dev.restaurante SET endereco_bairro = 'Centro' WHERE id = 3;
+UPDATE food4dev.restaurante SET endereco_cidade = 'Florianópolis' WHERE id = 3;
+UPDATE food4dev.restaurante SET endereco_estado = 'Santa catarina' WHERE id = 3;
+
+UPDATE food4dev.restaurante SET endereco_rua = 'Av manoel ribas' WHERE id = 4;
+UPDATE food4dev.restaurante SET endereco_numero = 750 WHERE id = 4;
+UPDATE food4dev.restaurante SET endereco_bairro = 'Boa vista' WHERE id = 4;
+UPDATE food4dev.restaurante SET endereco_cidade = 'Curitiba' WHERE id = 4;
+UPDATE food4dev.restaurante SET endereco_estado = 'Parana' WHERE id = 4;
+
+UPDATE food4dev.restaurante SET endereco_rua = 'São luiz' WHERE id = 5;
+UPDATE food4dev.restaurante SET endereco_numero = 50 WHERE id = 5;
+UPDATE food4dev.restaurante SET endereco_bairro = 'Damaceno' WHERE id = 5;
+UPDATE food4dev.restaurante SET endereco_cidade = 'Porto alegre' WHERE id = 5;
+UPDATE food4dev.restaurante SET endereco_estado = 'Rio grande do sul' WHERE id = 5;
+
+ALTER TABLE food4dev.restaurante DROP COLUMN endereco;
+SELECT * from food4dev.restaurante;
+-- [M2S07] Ex 7 - Refatoração das entidades
+ALTER TABLE food4dev.prato ADD COLUMN id SERIAL PRIMARY KEY;
+SELECT * FROM food4dev.prato;
+
+-- [M2S07] Ex 8 - Relacionando entidades
+ALTER TABLE food4dev.prato ADD COLUMN restaurante_id INT;
+ALTER TABLE food4dev.prato ADD CONSTRAINT restaurante_id FOREIGN KEY (restaurante_id) REFERENCES food4dev.restaurante (id);
+-- [M2S07] Ex 9 - Mais relacionamentos
+ALTER TABLE food4dev.categoria RENAME TO food4dev.categorias;
+ALTER TABLE food4dev.prato RENAME TO food4dev.pratos;
+ALTER TABLE food4dev.restaurante RENAME TO food4dev.restaurantes;
+DELETE FROM food4dev.categorias WHERE id >= 6;
+INSERT INTO food4dev.categorias (titulo)(SELECT r.categoria FROM food4dev.restaurantes r)
+ALTER TABLE food4dev.restaurantes RENAME COLUMN food4dev.categoria TO categoria_old;
+ALTER TABLE  food4dev.pratos  RENAME COLUMN food4dev.categoria TO categoria_old ;
+
+ALTER TABLE  food4dev.restaurantes ADD COLUMN  categoria_id INT;
+ALTER TABLE food4dev.restaurantes ADD CONSTRAINT fk_rest_cat FOREIGN KEY (categoria_id) REFERENCES categorias (id) MATCH FULL;
+
+UPDATE 
+    food4dev.restaurantes
+SET categoria_id = c.id 
+FROM categorias c
+WHERE categoria_old = c.titulo;
+ALTER TABLE  food4dev.pratos  ADD COLUMN  categoria_id INT;
+ALTER TABLE food4dev.pratos  ADD CONSTRAINT fk_prato_cat FOREIGN KEY (categoria_id) REFERENCES categorias (id) MATCH FULL;
+ALTER TABLE food4dev.pratos DROP COLUMN categoria_old ;
+ALTER TABLE food4dev.pratos DROP COLUMN url_foto;
+    
+UPDATE pratos p
+SET categoria_id = r.categoria_id 
+FROM restaurantes r 
+WHERE r.id = p.restaurante_id;
+-- [M2S07] Ex 10 - Queries
+
+-- a) Quantos restaurantes estão cadastrados no sistema?
+SELECT count(*) FROM food4dev.restaurantes r -- 5
+--b) Quantos pratos estão cadastrados no sistema?
+SELECT COUNT(*) FROM food4dev.pratos --15
+--c) Quantos pratos estão vinculados ao restaurante de ID 4?
+SELECT COUNT(*) FROM food4dev.pratos p WHERE p.restaurante_id = 4; -- 3
+-- d) Quantos pratos estão sem vinculação a nenhum restaurante?
+SELECT COUNT(*) FROM food4dev.pratos p WHERE p.restaurante_id ISNULL; -- 4
+-- e) Usando uma query só, quantos pratos e restaurantes estão vinculados à categoria de id 1 ?
+SELECT
+    count(*)
+FROM
+    categorias c
+INNER JOIN restaurantes r ON
+    c.id = r.id
+INNER JOIN pratos p ON
+    p.restaurante_id = c.id
+WHERE
+    c.id = 1; -- 3
+
+
 
