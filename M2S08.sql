5017c743f9a9c2a4a698348b7ae67bd5fcf079e3
Daniel Meireles <daniel@inovadora.com.br>
2022-02-12 12:09:54 -0300

    Finalizando exercicio 9

--- M2S08.sql
+++ M2S08.sql
@@ -95,3 +95,39 @@ AND e.bairro = endereco_bairro
 AND e.cidade = endereco_cidade
 AND e.uf = endereco_estado;
 
+-- [M2S08] Ex 9 - Continuação do Food4Devs
+ALTER TABLE restaurantes 
+DROP COLUMN endereco_rua,
+DROP COLUMN endereco_numero,
+DROP COLUMN endereco_comp,
+DROP COLUMN endereco_bairro,
+DROP COLUMN endereco_cidade,
+DROP COLUMN endereco_estado;
+
+INSERT INTO endereco (rua, numero, bairro, cidade, uf, complemento)
+VALUES 
+('Santos dumont', '1045', 'Bela Vista', 'Cuiabá', 'Mato grosso', 'Terreo 5'),
+('São jose', '1453', 'Vila nova', 'Arapiraca', 'Alagoas', 'Conjunto 10'),
+('Santo Antonio', '376', 'Planalto', 'Juiz de fora', 'Minas gerais', 'Sobreloja 10'),
+('Ceará', '7877', 'Santo Antonio', 'Bacabal', 'Maranhão', 'Loja 8'),
+('Espírito Santo', '1220', 'São Cristóvão', 'Sete Lagoas', 'Minas Gerais', 'Galeria 6'),
+('Maranhão', '5679', 'Boa Vista', 'Senador Canedo', 'Goiás', 'Loja 4')
+('Alagoas', '475', 'Santo Antônio', 'Cariacica', 'Espírito Santo', 'Andar 10'),
+('Santos Dumont', '3278', 'Santo Antônio', 'Luziânia', 'Goiás', 'Cobertura 8'),
+('Tiradentes', '369', 'São Francisco', 'Altamira', 'Pará', 'Terreo 1'),
+('Santa Maria', '2920', 'Industrial', 'Santarém', 'Pará','Quadra 2'),
+('Belo Horizonte', '2365', 'Centro', 'Timon', 'Maranhão', 'Sala 4'),
+('Alagoas', '9149', 'Bela Vista', 'Campina Grande', 'Paraíba', 'Conjunto 4');
+
+INSERT INTO clientes (nome) 
+VALUES
+('João', 3),
+('Maria', 10),
+('Alberta', 19),
+('Dolores', 18),
+('Jack', 14),
+('Tadeu', 12),
+('Karolina', 8),
+('Carolina', 13),
+('Margarete', 17),
+('Geruza', 16);
